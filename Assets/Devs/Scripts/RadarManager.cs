564da57a7c30b3410e814259b34a23c152c8ead1
Nameyho <37048681+Nameyho@users.noreply.github.com>
2021-05-10 17:02:04 +0200

    radarmanager spawn

--- Assets/Devs/Scripts/RadarManager.cs
+++ Assets/Devs/Scripts/RadarManager.cs
@@ -5,7 +5,51 @@ using UnityEngine;
 public class RadarManager : MonoBehaviour
 {
 
+    #region fields
+
+    private Boat[,] _tabBoat = new Boat[4, 8];
+    private float _nextspawnTime;
+    [SerializeField] private float _turnTime;
+    [SerializeField] private Boat _boatPrefab;
+    #endregion
+
+    #region Unity methods
+    private void Update()
+    {
+        SpawnBoat();
+        MoveBoat();
 
 
+
+    }
+    #endregion
+
+    #region privates methodes
+
+    private void SpawnBoat()
+    {
+
+        if (Time.time > _nextspawnTime  )
+        {
+
+            int index = Random.Range(0, 8);
+            if (_tabBoat[3, index] == null)
+            {
+                Boat tempBoat = Instantiate<Boat>(_boatPrefab);
+                _tabBoat[3, index] = tempBoat;
+            }
+        }
+
+        if (Time.time >= _nextspawnTime)
+        {
+            _nextspawnTime = Time.time + _turnTime/8;
+        }
+
+    } 
+
+    private void MoveBoat()
+    {
+    }
+    #endregion
 
 }
